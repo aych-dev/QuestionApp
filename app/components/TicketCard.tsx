7e5b23f32c506ea8453a007b5409c690a8b03ca8
aych <aychdev1@gmail.com>
2024-02-15 17:21:10 -0500

    updated priority

--- app/components/TicketCard.tsx
+++ app/components/TicketCard.tsx
@@ -5,9 +5,15 @@ import StatusDisplay from './StatusDisplay';
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className='flex flex-col bg-blue-400 hover:bg-blue-200 rounded-md shadow-lg p-3 m-2'>
+      <div className='flex mb-3'>
+        <div className='ml-2'>
+          <PriorityDisplay />
+        </div>
+        <div className='ml-auto'>
+          <DeleteBlock />
+        </div>
+      </div>
       <ProgressDisplay />
       <StatusDisplay />
     </div>
