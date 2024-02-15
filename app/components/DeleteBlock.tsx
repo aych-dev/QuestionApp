44558aef7b4b35d99c35fcfbeba4e606b7f51a6f
aych <aychdev1@gmail.com>
2024-02-14 19:41:47 -0500

    added prioritydisplay

--- app/components/DeleteBlock.tsx
+++ app/components/DeleteBlock.tsx
@@ -1,6 +1,6 @@
 const DeleteBlock = () => {
   return (
-    <p className='text-red-400 hover:cursor-pointer hover:text-red-200'>
+    <p className='text-red-400 hover:cursor-pointer hover:text-red-200 '>
       Delete
     </p>
   );
