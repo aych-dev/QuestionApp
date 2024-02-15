45f195e7ed66ecd3d2e6a9afafbad55849c45493
aych <aychdev1@gmail.com>
2024-02-15 16:36:43 -0500

    updated statusdisplay

--- app/components/StatusDisplay.tsx
+++ app/components/StatusDisplay.tsx
@@ -1,5 +1,9 @@
 const StatusDisplay = () => {
-  return <div>StatusDisplay</div>;
+  return (
+    <span className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-300'>
+      DONE
+    </span>
+  );
 };
 
 export default StatusDisplay;
