7f013767fa60fdb66ea1c850c7b3a93e9c2f2af5
aych <aychdev1@gmail.com>
2024-02-26 17:21:07 -0500

    updated component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import TicketCard from './components/TicketCard';
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/Tickets', {
+      cache: 'no-store',
+    });
+
+    return res.json();
+  } catch (error) {
+    console.log('failed to get tickets', error);
+  }
+};
+
+interface CategoryTypes {
+  _id: string;
+  title: string;
+  category: string;
+  priority: number;
+  progress: number;
+  status: string;
+  active: boolean;
+}
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  const uniqueCategories: string[] = [
+    ...new Set(tickets?.map(({ category }: any) => category) as string[]),
+  ];
+
   return (
     <div className='p-5'>
-      <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => {
+            return (
+              <div key={categoryIndex} className='mb-4'>
+                <h2>{uniqueCategory}</h2>
+                <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
+                  {tickets
+                    .filter(
+                      (ticket: CategoryTypes) =>
+                        ticket.category === uniqueCategory
+                    )
+                    .map((filteredTicket: string, _index: number) => {
+                      return (
+                        <TicketCard
+                          id={_index}
+                          key={_index}
+                          ticket={filteredTicket}
+                        />
+                      );
+                    })}
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
