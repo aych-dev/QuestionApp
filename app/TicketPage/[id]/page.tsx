3b6ad6836a9d9c955a9c1dfada74f13b094cd2a0
aych <aychdev1@gmail.com>
2024-03-05 12:48:58 -0500

    new function added

--- app/TicketPage/[id]/page.tsx
+++ app/TicketPage/[id]/page.tsx
@@ -1,6 +1,26 @@
 import TicketForm from '@/app/components/TicketForm';
 
-const TicketPage = ({ params }: any) => {
+const getTicketById = async (id: string) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: 'no-store',
+    });
+    return res.json();
+  } catch (error) {
+    throw new Error('Failed to get Ticket');
+  }
+};
+
+const TicketPage = async ({ params }: any) => {
+  const EDITMODE = params.id === 'new' ? false : true;
+
+  let updateTicketData = {};
+
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+    console.log(updateTicketData);
+  }
+
   return <TicketForm />;
 };
 
